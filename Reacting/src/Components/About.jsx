8c19e3bc6c94d57c349d7af9543ac3ea263063a8
Devansh sharma <115212222+Devansh042005@users.noreply.github.com>
2025-05-07 23:42:28 +0530

    Add files via upload

---
+++ Reacting/src/Components/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <div className=' w-1/2 m-auto mt-10'>
+      <h1 className='text-red-200 text-5xl mb-2'>About</h1>
+      <p className='mb-2 text-zinc-500'>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima distinctio iusto cumque qui minus, quam totam, culpa autem, eos illum nisi officiis reprehenderit ab repellat.
+      </p>
+      <button className='text-white p-3 bg-zinc-200'>Explore More</button>
+    </div>
+    </div>
+  )
+}
+
+export default About
