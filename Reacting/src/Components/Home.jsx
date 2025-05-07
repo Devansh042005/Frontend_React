288c7233b834e3f725dee8d746e4e761f803d2bc
Devansh sharma <115212222+Devansh042005@users.noreply.github.com>
2025-05-07 23:43:21 +0530

    Add files via upload

---
+++ Reacting/src/Components/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className=' w-1/2 m-auto mt-10'>
+      <h1 className='text-red-200 text-5xl mb-2'>Home</h1>
+      <p className='mb-2 text-zinc-500'>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima distinctio iusto cumque qui minus, quam totam, culpa autem, eos illum nisi officiis reprehenderit ab repellat.
+      </p>
+      <button className='text-white p-3 bg-zinc-200'>Explore More</button>
+    </div>
+  )
+}
+
+export default Home
